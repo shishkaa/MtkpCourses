98fe0b137f083e6e884b85b30930b12dcfdb18b2
Poliak-steam <47054153+Poliak-steam@users.noreply.github.com>
2019-10-24 18:57:21 +0300

    Ахметов Тимур (22.10.2019)

---
+++ Programms/team2/Домашнее задание 1(Ахметов Тимур).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+	int mass[10];
+	int rev, min, max;
+
+	for (int i = 0; i < 10; i++) {
+		mass[i] = rand() % 50;
+		cout << mass[i] << endl;
+	}
+
+	min = mass[0];
+	max = mass[0];
+
+	for (int i = 0; i < 10; i++) {
+		if (min > mass[i])
+			min = mass[i];
+
+		if (max < mass[i])
+			max = mass[i];
+	}
+	cout << "минимальное значение: " << min << endl;
+	cout << "максимальное значение: " << max;
+}
