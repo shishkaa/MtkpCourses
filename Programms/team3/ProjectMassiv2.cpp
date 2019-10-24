984b1d0cf995c4ac9f3417a3f0a62991e6fec9bc
Konstantin-41 <56935673+Konstantin-41@users.noreply.github.com>
2019-10-24 12:50:55 +0300

    Забнев К.А

---
+++ Programms/team3/ProjectMassiv2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int sum = 0;
+	int i;
+	int mas[3];
+	for (i = 0; i < 3; i++) {
+		cin >> mas[i];
+		while (mas[i] != 0) {
+			sum += mas[i] % 10;
+			mas[i] /= 10;
+		}
+	}
+	cout << sum;
+}
+
