5136f4b133481620d8354c3c59a939ae662f54c6
Poliak-steam <47054153+Poliak-steam@users.noreply.github.com>
2019-10-24 17:09:06 +0300

    Add files via upload

---
+++ Домашнее задание 1(Ахметов Тимур).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float num;
+	float sum = 0;
+	float sr;
+	for (int i = 1; i > 0; i++) {
+		cout << "введите число: ";
+		cin >> num;
+		sum += num;
+		if (i > 1) {
+			sr = sum / (i - 1);
+		}
+		else {
+			sr = sum / i;
+		}
+
+		if (num == 0) {
+
+			cout << "количество неверных попыток: " << i-1 << endl;
+			cout << "сумма всех попыток: " << sum << endl;
+			cout << "среднее ариф.: " << sr - 1;
+			break;
+		}
+	}
+}
