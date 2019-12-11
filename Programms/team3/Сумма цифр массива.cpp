89f5457e60484a6c6de3fa0ccae74b6d0cc49f53
Aleksandra <56135700+shishkaa@users.noreply.github.com>
2019-12-11 13:23:41 +0300

    Update Сумма цифр массива.cpp

--- Programms/team3/Сумма цифр массива.cpp
+++ Programms/team3/Сумма цифр массива.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-	int sum = 0;
-	int i;
-	int mas[3];
-	for (i = 0; i < 3; i++) {
-		cin >> mas[i];
-		while (mas[i] != 0) {
-			sum += mas[i] % 10;
-			mas[i] /= 10;
-		}
-	}
-	cout << sum;
+int sum = 0;
+int i;
+int mas[3];
+for (i = 0; i < 3; i++) {
+cin >> mas[i];
+while (mas[i] != 0) {
+sum += mas[i] % 10;
+mas[i] /= 10;
+}
+}
+cout << sum;
 }
